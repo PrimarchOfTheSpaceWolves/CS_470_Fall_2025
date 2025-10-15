436e2d6108d4bfd2f78523b31e0d4a8132c5dd39
Dr. Michael J. Reale <realemj@sunypoly.edu>
2025-10-15 17:51:10 -0400

    ML example up to MDC

--- ProfExercise_ML.py
+++ ProfExercise_ML.py
@@ -3,8 +3,27 @@ import numpy as np
 import pandas
 import sklearn
 from torchvision import datasets
+import time
 
-from sklearn.neighbors import KNeighborsClassifier
+from sklearn.neighbors import (KNeighborsClassifier, NearestCentroid)
+from sklearn.metrics import (accuracy_score, f1_score, 
+                             roc_auc_score, confusion_matrix, 
+                             classification_report)
+import torch
+import torch.nn.functional as F
+
+def compute_metrics(ground, pred, prob, class_cnt):
+    accuracy = accuracy_score(ground, pred)
+    f1 = f1_score(ground, pred, average="macro")
+    
+    one_hot_ground = F.one_hot(torch.from_numpy(ground), num_classes=class_cnt).numpy()
+    auc = roc_auc_score(one_hot_ground, prob, average="macro", multi_class="ovr")
+    
+    return {
+        "Accuracy": accuracy,
+        "F1-score": f1,
+        "AUC": auc
+    }
 
 def convert_images(data):
     data = data.numpy()
@@ -42,15 +61,42 @@ def main():
     #print(x_train[0].shape)
     #display_image("IMAGE", x_train[0])
     
-    classifier = KNeighborsClassifier(n_neighbors=3, weights="uniform")
+    start_time = time.time()
+    #classifier = KNeighborsClassifier(n_neighbors=3, 
+    #                                  weights="uniform", 
+    #                                  algorithm="auto",
+    #                                  n_jobs=None)
+    classifier = NearestCentroid()
     classifier.fit(x_train, y_train)
-    print("Training done.")
+    end_time = time.time()
+    print("Training done:", (end_time-start_time), "seconds")
     
+    start_time = time.time()
     pred_train = classifier.predict(x_train)
     pred_test = classifier.predict(x_test)
+    end_time = time.time()
+    print("Prediction done:", (end_time-start_time), "seconds")
     
+    prob_train = classifier.predict_proba(x_train)
+    prob_test = classifier.predict_proba(x_test)
     
+    print(pred_train.shape)
+    print(prob_train.shape)
     
+    class_cnt = 10
+    train_metrics = compute_metrics(y_train, pred_train, prob_train, class_cnt)
+    test_metrics = compute_metrics(y_test, pred_test, prob_test, class_cnt)
+    
+    print("TRAINING:")
+    print(train_metrics)
+    print("TESTING:")
+    print(test_metrics)
+    
+    print("CONFUSION MATRIX (train):")
+    print(confusion_matrix(y_train, pred_train))
+    
+    print("CLASSIFICATION REPORT (train):")
+    print(classification_report(y_train, pred_train))
     
     
     
