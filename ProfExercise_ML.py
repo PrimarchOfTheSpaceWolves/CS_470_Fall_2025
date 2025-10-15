3d0dfce9cd92e055f1312b9f81940d816695ef80
Dr. Michael J. Reale <realemj@sunypoly.edu>
2025-10-15 17:01:55 -0400

    ML exercise so far

---
+++ ProfExercise_ML.py
@@ -0,0 +1,61 @@
+import cv2
+import numpy as np
+import pandas
+import sklearn
+from torchvision import datasets
+
+from sklearn.neighbors import KNeighborsClassifier
+
+def convert_images(data):
+    data = data.numpy()
+    data = data.astype("float32")
+    data /= 255.0
+    data -= 0.5
+    data *= 2.0
+    return data
+
+def display_image(label, image):
+    image = cv2.resize(image, dsize=None, fx=5.0, fy=5.0)
+    cv2.imshow(label, image)
+    cv2.waitKey(-1)
+    cv2.destroyAllWindows()
+
+def main():
+    
+    train_ds = datasets.MNIST(root="./data", train=True, download=True)
+    test_ds = datasets.MNIST(root="./data", train=False, download=True)
+    
+    x_train = convert_images(train_ds.data)
+    x_test = convert_images(test_ds.data)
+    
+    x_train = np.reshape(x_train, (x_train.shape[0], -1))
+    x_test = np.reshape(x_test, (x_test.shape[0], -1))
+    
+    y_train = train_ds.targets.numpy()
+    y_test = test_ds.targets.numpy()
+    
+    print(x_train.shape)
+    print(x_test.shape)
+    print(y_train.shape)
+    print(y_test.shape)
+    
+    #print(x_train[0].shape)
+    #display_image("IMAGE", x_train[0])
+    
+    classifier = KNeighborsClassifier(n_neighbors=3, weights="uniform")
+    classifier.fit(x_train, y_train)
+    print("Training done.")
+    
+    pred_train = classifier.predict(x_train)
+    pred_test = classifier.predict(x_test)
+    
+    
+    
+    
+    
+    
+    
+
+if __name__ == "__main__":
+    main()
+    
